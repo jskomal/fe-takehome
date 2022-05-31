e06d28bddcd66b95a6331cfb006ad6f193f3755f
Jordan Skomal <jskomal@gmail.com>
2022-05-31 13:59:29 -0600

    Add basic fetch Call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  return <div className='App'></div>
+  const [articles, setArticles] = useState<any>(null)
+
+  useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, [])
+
+  return (
+    <div className='App'>
+      <h1>ARTS NEWS ALERT</h1>
+    </div>
+  )
 }
 
 export default App
